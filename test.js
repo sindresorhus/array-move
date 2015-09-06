fc9f2ee91a0f094c2786c08cfa0b5c57ec3445be
disrupticons <disrupticons@gmail.com>
2015-09-07 03:00:10 +0530

    added tests for negative 'to' argument

--- test.js
+++ test.js
@@ -6,10 +6,16 @@ var fixture = [1, 2, 3, 4, 5];
 test(function (t) {
 	t.same(fn(fixture, 3, 0), [4, 1, 2, 3, 5]);
 	t.same(fn(fixture, -1, 0), [5, 1, 2, 3, 4]);
+	t.same(fn(fixture, 1, -2), [1, 3, 4, 2, 5]);
+	t.same(fn(fixture, -3, -4), [1, 3, 2, 4, 5]);
 
 	var fixture2 = fixture.slice();
 	fn.mut(fixture2, 3, 0);
 	t.same(fixture2, [4, 1, 2, 3, 5]);
+
+	var fixture3 = fixture.slice();
+	fn.mut(fixture3, 1, -2);
+	t.same(fixture3, [1, 3, 4, 2, 5]);
 
 	t.end();
 });
