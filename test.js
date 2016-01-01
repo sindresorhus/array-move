2108cc3b49f9c877ea9d049002886ffaa1aaf065
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 14:52:10 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 import test from 'ava';
-import fn from './';
-const fixture = [1, 2, 3, 4, 5];
+import m from './';
+
+const f = [1, 2, 3, 4, 5];
 
 test(t => {
-	t.same(fn(fixture, 3, 0), [4, 1, 2, 3, 5]);
-	t.same(fn(fixture, -1, 0), [5, 1, 2, 3, 4]);
-	t.same(fn(fixture, 1, -2), [1, 3, 4, 2, 5]);
-	t.same(fn(fixture, -3, -4), [1, 3, 2, 4, 5]);
+	t.same(m(f, 3, 0), [4, 1, 2, 3, 5]);
+	t.same(m(f, -1, 0), [5, 1, 2, 3, 4]);
+	t.same(m(f, 1, -2), [1, 3, 4, 2, 5]);
+	t.same(m(f, -3, -4), [1, 3, 2, 4, 5]);
 
-	const fixture2 = fixture.slice();
-	fn.mut(fixture2, 3, 0);
-	t.same(fixture2, [4, 1, 2, 3, 5]);
-
-	t.end();
+	const f2 = f.slice();
+	m.mut(f2, 3, 0);
+	t.same(f2, [4, 1, 2, 3, 5]);
 });
