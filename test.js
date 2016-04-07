2ba22bcf4b678513272d3e0aedba9e13b0affeb4
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 13:56:30 +0700

    upgrade to latest AVA

--- test.js
+++ test.js
@@ -4,12 +4,12 @@ import m from './';
 const f = [1, 2, 3, 4, 5];
 
 test(t => {
-	t.same(m(f, 3, 0), [4, 1, 2, 3, 5]);
-	t.same(m(f, -1, 0), [5, 1, 2, 3, 4]);
-	t.same(m(f, 1, -2), [1, 3, 4, 2, 5]);
-	t.same(m(f, -3, -4), [1, 3, 2, 4, 5]);
+	t.deepEqual(m(f, 3, 0), [4, 1, 2, 3, 5]);
+	t.deepEqual(m(f, -1, 0), [5, 1, 2, 3, 4]);
+	t.deepEqual(m(f, 1, -2), [1, 3, 4, 2, 5]);
+	t.deepEqual(m(f, -3, -4), [1, 3, 2, 4, 5]);
 
 	const f2 = f.slice();
 	m.mut(f2, 3, 0);
-	t.same(f2, [4, 1, 2, 3, 5]);
+	t.deepEqual(f2, [4, 1, 2, 3, 5]);
 });
