05221714e5fbf0d1c9e1a47b9a8a96c5d2e55ee3
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-20 16:53:15 +0000

    Require Node.js 8, add TypeScript definition (#5)

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
-function arrayMove(x, from, to) {
-	x.splice((to < 0 ? x.length + to : to), 0, x.splice(from, 1)[0]);
-}
-
-module.exports = function (x, from, to) {
-	x = x.slice();
-	arrayMove(x, from, to);
-	return x;
+const arrayMoveMutating = (array, from, to) => {
+	array.splice((to < 0 ? array.length + to : to), 0, array.splice(from, 1)[0]);
 };
 
-module.exports.mut = arrayMove;
+const arrayMove = (array, from, to) => {
+	array = array.slice();
+	arrayMoveMutating(array, from, to);
+	return array;
+};
+
+module.exports = arrayMove;
+module.exports.default = arrayMove;
+
+module.exports.mut = arrayMoveMutating;
---
+++ index.test-d.ts
@@ -0,0 +1,6 @@
+import {expectType} from 'tsd-check';
+import arrayMove from '.';
+
+expectType<string[]>(arrayMove(['a', 'b', 'c'], 1, 2));
+expectType<number[]>(arrayMove([1, 2, 3], 1, 2));
+expectType<void>(arrayMove.mut(['a', 'b', 'c'], 1, 2));
