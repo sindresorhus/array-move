c97e4f1390b836c429fe4c268e59d8cd2d733cba
xcsrz <xcsrz@users.noreply.github.com>
2019-11-26 16:28:28 -0800

    IE fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const arrayMoveMutate = (array, from, to) => {
-	array.splice(to < 0 ? array.length + to : to, 0, array.splice(from, 1)[0]);
+	array.splice((to < 0 ? array.length + to : to), 0, array.splice(from, 1)[0]);
 };
 
 const arrayMove = (array, from, to) => {
