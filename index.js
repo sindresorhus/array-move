50e24d01b50632946452875e69c9475d915071c9
Sindre Sorhus <sindresorhus@gmail.com>
2019-12-01 23:56:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const arrayMoveMutate = (array, from, to) => {
-	const item = array.splice(from, 1)[0];
 	const startIndex = to < 0 ? array.length + to : to;
+	const item = array.splice(from, 1)[0];
 	array.splice(startIndex, 0, item);
 };
 
