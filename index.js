064a79cddd973edacc210ad88208af499227ad3a
Richie Bendall <richiebendall@gmail.com>
2020-07-13 02:00:51 +1200

    Ignore invalid indexes and require Node.js 10 (#14)

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 const arrayMoveMutate = (array, from, to) => {
 	const startIndex = to < 0 ? array.length + to : to;
-	const item = array.splice(from, 1)[0];
-	array.splice(startIndex, 0, item);
+
+	if (startIndex >= 0 && startIndex < array.length) {
+		const item = array.splice(from, 1)[0];
+		array.splice(startIndex, 0, item);
+	}
 };
 
 const arrayMove = (array, from, to) => {
-	array = array.slice();
+	array = [...array];
 	arrayMoveMutate(array, from, to);
 	return array;
 };
 
 module.exports = arrayMove;
-// TODO: Remove this for the next major release
-module.exports.default = arrayMove;
-
 module.exports.mutate = arrayMoveMutate;
--- test.js
+++ test.js
@@ -8,10 +8,11 @@ test('main', t => {
 	t.deepEqual(arrayMove(fixture, -1, 0), [5, 1, 2, 3, 4]);
 	t.deepEqual(arrayMove(fixture, 1, -2), [1, 3, 4, 2, 5]);
 	t.deepEqual(arrayMove(fixture, -3, -4), [1, 3, 2, 4, 5]);
+	t.deepEqual(arrayMove(fixture, 5, 6), [1, 2, 3, 4, 5]);
 });
 
 test('.mutate', t => {
-	const fixture2 = fixture.slice();
+	const fixture2 = [...fixture];
 	arrayMove.mutate(fixture2, 3, 0);
 	t.deepEqual(fixture2, [4, 1, 2, 3, 5]);
 });
