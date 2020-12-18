8d849df597501c3585d5aa43b1eb067602d7343a
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2020-12-19 00:52:55 +0800

    Add esm support

--- index.d.ts
+++ index.d.ts
@@ -29,6 +29,35 @@ declare const arrayMove: {
 	<ValueType>(array: ReadonlyArray<ValueType>, from: number, to: number): ValueType[];
 
 	/**
+	Clones the given `array`, moves the item to a new position in the new array, and then returns the new array. The given `array` is not mutated.
+
+	@param array - The array with the item to move.
+	@param from - Index of item to move. If negative, it will begin that many elements from the end.
+	@param to - Index of where to move the item. If negative, it will begin that many elements from the end.
+	@returns A new array with the item moved to the new position.
+
+	@example
+	```
+	import arrayMove = require('array-move');
+
+	const input = ['a', 'b', 'c'];
+
+	const array1 = arrayMove(input, 1, 2);
+	console.log(array1);
+	//=> ['a', 'c', 'b']
+
+	const array2 = arrayMove(input, -1, 0);
+	console.log(array2);
+	//=> ['c', 'a', 'b']
+
+	const array3 = arrayMove(input, -2, -3);
+	console.log(array3);
+	//=> ['b', 'a', 'c']
+	```
+	*/
+	default: <ValueType>(array: ReadonlyArray<ValueType>, from: number, to: number) => ValueType[];
+
+	/**
 	Moves the item to the new position in the input array. Useful for huge arrays where absolute performance is needed.
 
 	@param array - The array to modify.
---
+++ index.mjs
@@ -0,0 +1,21 @@
+'use strict';
+
+const arrayMoveMutate = (array, from, to) => {
+	const startIndex = from < 0 ? array.length + from : from;
+
+	if (startIndex >= 0 && startIndex < array.length) {
+		const endIndex = to < 0 ? array.length + to : to;
+
+		const [item] = array.splice(from, 1);
+		array.splice(endIndex, 0, item);
+	}
+};
+
+const arrayMove = (array, from, to) => {
+	array = [...array];
+	arrayMoveMutate(array, from, to);
+	return array;
+};
+
+export default arrayMove;
+export { arrayMoveMutate as mutate };
